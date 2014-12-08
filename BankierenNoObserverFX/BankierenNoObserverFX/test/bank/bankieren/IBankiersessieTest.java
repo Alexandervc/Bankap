66b4d07d664c87d5861ca7700d21486306bb99e2
xChocoMel <melaniekwetters@hotmail.com>
2014-12-08 12:26:21 +0100

    tesjes :smirk:

---
+++ BankierenNoObserverFX/BankierenNoObserverFX/test/bank/bankieren/IBankiersessieTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bank.bankieren;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Melanie
+ */
+public class IBankiersessieTest
+{
+    
+    public IBankiersessieTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConctructor()
+    {
+        //todo
+    }
+    
+    @Test
+    public void testIsGeldig()
+    {
+        //todo
+    }
+    
+    @Test
+    public void testMaakOver()
+    {
+        //todo  
+    }
+    
+    @Test
+    public void testLogUit()
+    {
+        //todo
+    }
+}
