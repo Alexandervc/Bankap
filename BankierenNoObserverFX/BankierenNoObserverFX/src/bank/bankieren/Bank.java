8187c3324ac6ff52cf57286b9ef067e0175bcd5c
Alexandervc <a.vancuijk@student.fontys.nl>
2014-12-15 12:31:00 +0100

    betere synchronized

--- BankierenNoObserverFX/BankierenNoObserverFX/src/bank/bankieren/Bank.java
+++ BankierenNoObserverFX/BankierenNoObserverFX/src/bank/bankieren/Bank.java
@@ -23,7 +23,7 @@ public class Bank implements IBank {
 		this.name = name;	
 	}
 
-	public int openRekening(String name, String city) {
+	public synchronized int openRekening(String name, String city) {
 		if (name == null || city == null || name.equals("") || city.equals(""))
 			return -1;
 
@@ -48,7 +48,7 @@ public class Bank implements IBank {
 		return accounts.get(nr);
 	}
 
-	public synchronized boolean maakOver(int source, int destination, Money money)
+	public boolean maakOver(int source, int destination, Money money)
 			throws NumberDoesntExistException {
 		if (source == destination)
 			throw new RuntimeException(
